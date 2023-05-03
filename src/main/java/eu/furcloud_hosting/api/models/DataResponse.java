adef05e1664d740ba95b8c953db9350993ea48f9
EwanFabiani <ewan.fabiani.c@gmail.com>
2023-05-03 13:38:11 +0200

    Small warning fixes

--- src/main/java/eu/furcloud_hosting/api/models/DataResponse.java
+++ src/main/java/eu/furcloud_hosting/api/models/DataResponse.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 public class DataResponse {
 
     @SerializedName("status")
-    private ResponseStatus status;
-    private HashMap<String, String> data;
+    private final ResponseStatus status;
+    private final HashMap<String, String> data;
 
     public DataResponse(ResponseStatus status, HashMap<String, String> data) {
         this.status = status;
--- src/main/java/eu/furcloud_hosting/api/services/RegexService.java
+++ src/main/java/eu/furcloud_hosting/api/services/RegexService.java
@@ -4,7 +4,7 @@ public class RegexService {
 
     public static final String USERNAME_REGEX = "^[a-zA-Z0-9]{4,24}$";
     public static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[a-zA-Z\\d\\w\\W]{8,256}$";
-    public static final String EMAIL_REGEX = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$";
+    public static final String EMAIL_REGEX = "^([a-zA-Z0-9_\\-.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$";
 
     public static boolean isValidUsername(String username) {
         return username.matches(USERNAME_REGEX);
