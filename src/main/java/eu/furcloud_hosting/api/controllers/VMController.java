fcdce54c40bcf4cf74807a54b79c13b860955404
EwanFabiani <ewan.fabiani.c@gmail.com>
2023-05-04 12:41:52 +0200

    idk too tired for title

--- src/main/java/eu/furcloud_hosting/api/controllers/VMController.java
+++ src/main/java/eu/furcloud_hosting/api/controllers/VMController.java
@@ -1,5 +1,7 @@
 package eu.furcloud_hosting.api.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/vm")
 public class VMController {
 
-
+    @GetMapping("/os")
+    public ResponseEntity<String> getOS() {
+        return null;
+    }
 
 }
