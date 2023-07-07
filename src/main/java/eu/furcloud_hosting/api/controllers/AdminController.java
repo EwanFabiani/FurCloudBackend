d736fc1171af748fdde9663af04875efb7f464e4
EwanFabiani <ewan.fabiani.c@gmail.com>
2023-07-07 09:55:31 +0200

    Added Admin Controller

---
+++ src/main/java/eu/furcloud_hosting/api/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package eu.furcloud_hosting.api.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/admin", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminController {
+
+
+
+}
